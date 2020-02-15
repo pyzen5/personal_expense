7a17ed65b09f3e346ca9e8c4e2427f2732bee0de
pyzen5 <pyzen55@gmail.com>
2020-02-15 22:52:12 +0530

    container styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Weekly Grocery', amount: 16.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +39,25 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('List of Transactions!!'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
+                    child: Text(
+                      tx.amount.toString(),
+                    ),
+                  ),
+                  Column(children: <Widget>[
+                    Text(tx.title),
+                    Text(tx.date.toString()),
+                  ])
+                ]),
+              );
+            }).toList(),
           ),
         ],
       ),
